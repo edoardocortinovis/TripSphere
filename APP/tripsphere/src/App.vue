cdcdbd33b8c9d47255aa5863a0c5b8d46a790311
edoardo cortinovis <cortinovis.edoardo.studente@itispaleocapa.it>
2024-11-21 23:01:02 +0100

    cambio font

--- APP/tripsphere/src/App.vue
+++ APP/tripsphere/src/App.vue
@@ -16,7 +16,7 @@ export default {
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Inter', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
