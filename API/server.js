c39fcb3f91b179517f19771cc79e255fc846184c
edoardo cortinovis <cortinovis.edoardo.studente@itispaleocapa.it>
2025-03-24 19:15:28 +0100

    docker funzionante

--- API/server.js
+++ API/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const sqlite3 = require('sqlite3');
+const sqlite3 = require('sqlite3').verbose();
 const cors = require('cors');
 require('dotenv').config();
 const session = require('express-session');
