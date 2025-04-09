85ce9bd230d121197c4f31a847aabbb776724efa
edoardo cortinovis <cortinovis.edoardo.studente@itispaleocapa.it>
2025-04-09 19:33:18 +0200

    cambiamento per rendere del config

--- APP/tripsphere/vue.config.js
+++ APP/tripsphere/vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',  // Questo permette al server di accettare richieste da qualsiasi host
+    allowedHosts: 'all', // Permette qualsiasi host (compreso Render o il tuo dominio personalizzato)
+  }
 })
